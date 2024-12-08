19bfe81858c364209ba612d2d86f79bb4fb4250a
Folkert Semplonius <folkert@semplonius.info>
2024-12-08 15:16:39 +0100

    Added day 7

---
+++ src/AOC_2407-1.php
@@ -0,0 +1,49 @@
+<?php
+
+$start = microtime(true);
+include 'Puzzle_2407.php';
+$input = explode("\r\n", $s1);
+
+$mulAdd = array ();
+
+while ($s1=next($input)) {
+  list ($res, $dig) = explode(': ', $s1);
+  $digits = explode(' ', $dig);
+  $n = count($digits) - 1;
+  for ($i=0; $i<$n; $i++) {
+    $mulAdd[$i] = '+';
+  }
+  $lastMul = 0;
+  while ($res != $resTot) {
+    $resTot = $digits[0];
+    for ($i=0; $i<$n; $i++) {
+      if ($mulAdd[$i] == '+')
+        $resTot += $digits[$i+1];
+      else
+        $resTot *= $digits[$i+1];
+    }
+    if ($resTot == $res) {
+      $tot += $res;
+      break;
+    }
+    else
+      $k = -1;
+    while (++$k<$n) {
+      if ($mulAdd[$k] == '+') {
+        $mulAdd[$k] = '*';
+        break;
+      }
+      else
+        $mulAdd[$k] = '+';
+    }
+    if ($k >= $n)
+      break;
+  }
+}
+
+
+echo "Result: $tot<br />\n";
+$time = round(microtime(true)-$start, 2);
+echo "Time: $time sec.";
+
+?>
